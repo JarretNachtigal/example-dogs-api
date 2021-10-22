366336460ddd3ff8502fd3e3e65704a1d4485323
Jarret Nachtigal <jarretn1224@gmail.com>
2021-10-22 15:32:47 -0400

    works aside from bonus

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,19 @@
 class DogsController < ApplicationController
+  def create
+    if current_user
+      dog = Dog.new(
+        name: params[:name],
+        age: params[:age],
+        breed: params[:breed]
+      )
+      #validation
+      if dog.save
+        render json: dog.as_json
+      else
+        render json: {message: "bad dog"}
+      end
+    else
+      render json: {message: "please log in mr gamer boi"}
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   post "/users" => "users#create"
+  post "/sessions" => "sessions#create"
+  post "/dogs" => "dogs#create"
 end
